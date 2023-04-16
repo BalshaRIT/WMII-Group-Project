98ba4d75bdfe2c8a73e43886b8b4c4f3b2747f0e
AleksaIvanovic1308 <129984476+AleksaIvanovic1308@users.noreply.github.com>
2023-04-16 16:18:31 +0200

    Add files via upload

--- group_project/assets/java_script/script.js
+++ group_project/assets/java_script/script.js
@@ -13,3 +13,26 @@ function scrollFunction() {
         makeAppear();
     };
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const mainDiv = document.getElementById("main-div");
+    const mainDivContent = mainDiv.innerHTML;
+    mainDiv.innerHTML = mainDivContent + mainDivContent;
+
+    const mainDivWidth = mainDiv.offsetWidth / 2;
+    const keyframes = `
+        @keyframes marquee {
+            0% {
+                transform: translateX(${mainDivWidth}px);
+            }
+            100% {
+                transform: translateX(-${mainDivWidth}px);
+            }
+        }
+    `;
+
+    const style = document.createElement("style");
+    style.innerHTML = keyframes;
+    document.head.appendChild(style);
+});
+
